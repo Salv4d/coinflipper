bee3ba1d824b810eef8dc465235528c90972948e
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-02-01 22:32:22 -0400

    add button to flip coin and a tracker of how many heads/tails/total have been

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -12,20 +12,29 @@ class CoinFlipper extends Component {
   }
 
   render() {
+    const { heads, tails } = this.state;
     return (
       <div className="CoinFlipper">
-        <Coin face={this.state.face} />
+        <div className="CoinFlipper-coin">
+          <Coin face={this.state.face} />
+        </div>
+        <button className="CoinFlipper-btn" onClick={this.flip}>
+          Flip
+        </button>
+        <p>
+          You had {heads} heads and {tails} tails out of {heads + tails} flips
+        </p>
       </div>
     );
   }
 
   flip() {
     const rand = Math.floor(Math.random() * 2);
-    this.setState((curState) => ({ face: this.coinFaces[rand] }));
-  }
-
-  componentDidMount() {
-    this.flip();
+    this.setState((curState) => ({
+      face: this.coinFaces[rand],
+      heads: curState.heads + (rand === 0 ? 1 : 0),
+      tails: curState.tails + (rand === 1 ? 1 : 0),
+    }));
   }
 }
 
